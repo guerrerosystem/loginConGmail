a9b1d10d55e49f19f7f7e2d657daf1b2d05b92e3
Milla guerrero E <78340596+guerrerosystem@users.noreply.github.com>
2021-07-20 14:08:09 -0500

    nuevo actualizacion

--- app/src/main/java/com/example/logincongoogle/MainActivity.java
+++ app/src/main/java/com/example/logincongoogle/MainActivity.java
@@ -4,15 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    CircleImageView img;
+    TextView tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        img = findViewById(R.id.image1);
+        tv1=findViewById(R.id.tv1);
+        tv2=findViewById(R.id.tv2);
+        if (user!=null){
+            String name=user.getDisplayName();
+            String gmail=user.getEmail();
+
+            tv1.setText("Usuario  " + name  );
+            tv2.setText(" Email " + gmail);
+            Picasso.get().load(user.getPhotoUrl()).placeholder(R.drawable.loginn).into(img);
+        }else {
+            getApplicationContext();
+
+        }
     }
     public void serra(View view){
         FirebaseAuth.getInstance().signOut();
